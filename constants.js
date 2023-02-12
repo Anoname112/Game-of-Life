0ec8e015ab3fd005ee81e2f95fd72b3b27e90872
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-02-12 16:56:07 +0700

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,25 @@
+const bodyBackColor = "#fff";
+const bodyTextColor = "#000";
+const bodyFont = "15px Segoe UI";
+
+const canvasBackColor = "#fff";
+const canvasPosition = "fixed";
+
+const msgTextColor = "#000";
+const msgFont = "15px Consolas";
+const msgPad = 50;
+const msgSpacing = 20;
+
+const interval = 1000 / 60;
+
+const bound = 400;				// Must be sqrt-able, for example: 9, 16, 25, 36, 49, 64, 81, 100, 121, ...
+const sqrt = Math.sqrt(bound);
+
+const tickChange = 5;
+const minTime = 10;
+const maxTime = 100;
+
+const squareWidth = 15;
+const squareHeight = 15;
+const squareColor = "#000";
+const borderColor = "#aaa";
---
+++ main.js
@@ -0,0 +1,164 @@
+var c;
+var ctx;
+
+var gen;
+var tTime;
+var tickTime = 30;
+var squares = new Array(bound);
+
+function initBodyCanvas () {
+	b = document.body;
+	b.style.background = bodyBackColor;
+	b.style.color = bodyTextColor;
+	b.style.font = bodyFont;
+	
+	c = document.getElementById("myCanvas");
+	c.style.background = canvasBackColor;
+	c.style.position = canvasPosition;
+	onResize();
+	c.style.left = (window.innerWidth - c.width) / 2;
+	c.style.top = (window.innerHeight - c.height) / 2;
+	
+	ctx = c.getContext("2d");
+}
+
+function initGame () {
+	gen = 0;
+	tTime = 0;
+	for (var i = 0; i < bound; i++) squares[i] = Math.floor(Math.random() * 2);
+}
+
+function getNeighbours (index) {
+	var neigh = 0;
+	
+	var u = index - sqrt;
+	var ul = index - sqrt - 1;
+	var ur = index - sqrt + 1;
+	var l = index - 1;
+	
+	var b = index + sqrt;
+	var bl = index + sqrt - 1;
+	var br = index + sqrt + 1;
+	var r = index + 1;
+	
+	var x = index % sqrt;
+	
+	var left = false;
+	if (x != 0) left = true;
+	var right = false;
+	if (x != sqrt - 1) right = true;
+	
+	if (u >= 0 && squares[u] == 1) neigh++;
+	if (ul >= 0 && left && squares[ul] == 1) neigh++;
+	if (ur >= 0 && right && squares[ur] == 1) neigh++;
+	if (l >= 0 && left && squares[l] == 1) neigh++;
+	
+	if (b < bound && squares[b] == 1) neigh++;
+	if (bl < bound && left && squares[bl] == 1) neigh++;
+	if (br < bound && right && squares[br] == 1) neigh++;
+	if (r < bound && right && squares[r] == 1) neigh++;
+	
+	return neigh;
+}
+
+function fillRect (x, y, w, h, s) {
+	ctx.fillStyle = s == null ? "#fff" : s;
+	ctx.fillRect(x, y, w, h);
+}
+
+function drawRect (x, y, w, h, s) {
+	ctx.strokeStyle = (s == null) ? "#000" : s;
+	ctx.strokeRect(x, y, w, h);
+	ctx.stroke();
+}
+
+function drawMessage (msg, x, y) {
+	ctx.font = msgFont;
+	ctx.fillStyle = msgTextColor;
+	ctx.fillText(msg, x, y + 12);
+}
+
+function draw () {
+	// Invalidate
+	c.width = c.width;
+	
+	// Prepare variables
+	var padX = (c.width - squareWidth * sqrt) / 2;
+	var padY = (c.height - squareHeight * sqrt) / 2;
+	
+	// Draw
+	drawMessage("Generation: " + gen, padX, padY - msgPad);
+	drawMessage("Tick Interval: " + tickTime, padX, padY - msgPad + msgSpacing);
+	for (var i = 0; i < bound; i++) {
+		var x = padX + (i % sqrt) * squareWidth;
+		var y = padY + Math.floor(i / sqrt) * squareHeight;
+		if (squares[i] == 1) fillRect(x, y, squareWidth, squareHeight, squareColor);
+		drawRect(x, y, squareWidth, squareHeight, borderColor);
+	}
+}
+
+function timerTick () {
+	if (tTime > 0) tTime--;
+	else {
+		// Draw current genertion
+		draw();
+		
+		// Prepare next generation
+		gen++;
+		var temp = new Array(bound);
+		for (var i = 0; i < bound; i++) {
+			var neigh = getNeighbours(i);
+			if (squares[i] == 1) {
+				// Live cell
+				if (neigh == 2 || neigh == 3) temp[i] = 1;
+				else temp[i] = 0;
+			}
+			else {
+				// Dead cell
+				if (neigh == 3) temp[i] = 1;
+				else temp[i] = 0;
+			}
+		}
+		squares = temp;
+		
+		// Reset timer
+		tTime = tickTime;
+	}
+}
+
+window.onload = function () {
+	window.onresize = onResize;
+	window.onkeydown = onKeyDown;
+	
+	initBodyCanvas();
+	initGame();
+	
+	setInterval(timerTick, interval);
+}
+
+function onResize () {
+	c.width = window.innerWidth;
+	c.height = window.innerHeight;
+}
+
+function onKeyDown (e) {
+	switch (e.keyCode) {
+		case 38:	// Arrow up
+			if (tickTime < maxTime)  {
+				tickTime += tickChange;
+				draw();
+			}
+			break;
+		case 40:	// Arrow down
+			if (tickTime > minTime) {
+				tickTime -= tickChange;
+				draw();
+			}
+			break;
+		case 82:	// R
+			initGame();
+			break;
+		default:
+			break;
+	}
+}
