27d294a8e13c2c9a8c65b1ed245b938f00352fe4
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-04-07 12:23:57 +0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+function getElement (id) {
+	return document.getElementById(id);
+}
+
 function fillRect (x, y, w, h, s) {
 	ctx.fillStyle = s == null ? "#fff" : s;
 	ctx.fillRect(x, y, w, h);
@@ -9,8 +13,18 @@ function drawRect (x, y, w, h, s) {
 	ctx.stroke();
 }
 
-function drawMessage (msg, x, y) {
+function drawMessage (msg, x, y, align) {
+	ctx.textAlign = (align == null) ? "start" : align;
 	ctx.font = msgFont;
 	ctx.fillStyle = msgTextColor;
 	ctx.fillText(msg, x, y + 12);
+	ctx.textAlign = "start";
+}
+
+function floor (value, floor) {
+	return Math.floor(value / floor) * floor;
+}
+
+function clamp (value, min, max) {
+	return Math.max(Math.min(value, max), min);
 }
